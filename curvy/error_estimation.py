4647baf67cfce26b456388d3151e39d8e6b2560f
Tobias Bettinger <40886857+tobias-bettinger@users.noreply.github.com>
2022-02-06 11:09:14 +0100

    error estimation method resolved

--- curvy/error_estimation.py
+++ curvy/error_estimation.py
@@ -6,11 +6,7 @@ def poly_quadratic(x, a, b):
 
 
 def get_error_bounds(curvature_array):
-    if isinstance(curvature_array, np.array):
-        error = poly_quadratic(curvature_array, 2.24, 0.15)
-        upper = curvature_array + error
-        lower = curvature_array - error
-        return lower, upper
-
-    else:
-        raise TypeError(f'Your input is not of type np.array --> {type(curvature_array)}')
+    error = poly_quadratic(curvature_array, 2.24, 0.15)
+    upper = curvature_array + error
+    lower = curvature_array - error
+    return lower, upper
